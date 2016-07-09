4a64b61fa09b7443a80ec61fae6bae768b9b8f58
flyover <flyover.gu@gmail.com>
2016-07-09 18:53:44 +0800

    修改界面

--- viewServer/static/js/index.js
+++ viewServer/static/js/index.js
@@ -1,21 +1,57 @@
 var primus = new Primus('http://127.0.0.1:8887');
 
 var logData = {};
+var filter = localStorage.filter || "";
 
 primus.on('data', function(data) {
     console.log(data);
     logData[data.id] = data;
-    renderItem(data);
+    if (~data.url.indexOf(filter)) {
+        renderItem(data);
+    }
 });
 
 var count = 1;
 var renderItem = function(data) {
     var template = $('#t-item').html();
     data.order = count++;
-    data.statusCode = data.res.statusCode;
+    if (data.res.statusCode == 1200) {
+        data.statusCode = '代理成功'
+    } else if (data.res.statusCode == 1500) {
+        data.statusCode = '代理失败'
+    } else {
+        data.statusCode = data.res.statusCode;
+    }
     var html = template.replace(/{{([^}]+)}}/g, function(a, b) {
         return data[b.trim()];
     });
     $('#item').append(html);
 }
 
+
+$('#item').on('click', '.look-req', function(e) {
+    var id = $(e.target).data('id');
+    renderDetail(logData[id].req);
+}).on('click', '.look-res', (e) => {
+    var id = $(e.target).data('id');
+    renderDetail(logData[id].res);
+});
+
+$('.clean-log').on('click', function() {
+    $('#item').html('');
+    logData = {};
+});
+
+$('.filter').on('keyup', function() {
+    filter = $('.filter').val();
+    localStorage.filter = filter;
+}).val(filter);
+
+var renderDetail = function(data) {
+    var proxyStr = JSON.stringify(data.proxy);
+    var headerStr = JSON.stringify(data.headers);
+    var bodyStr = JSON.stringify(data.body);
+    $('.header-view').text(headerStr);
+    $('.body-view').text(bodyStr);
+    $('.proxy-view').text(proxyStr);
+}
