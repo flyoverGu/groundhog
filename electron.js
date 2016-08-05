3c776fdf270af1a9c749a8139aeda341fa6f554b
flyover <flyover.gu@gmail.com>
2016-08-05 09:53:58 +0800

    docs: 修改package

--- electron.js
+++ electron.js
@@ -19,6 +19,9 @@ function createWindow() {
         height: 800
     })
 
+    // Open the DevTools.
+    // mainWindow.webContents.openDevTools()
+
     // and load the index.html of the app.
     mainWindow.loadURL('http://127.0.0.1:8887/index.html')
 
